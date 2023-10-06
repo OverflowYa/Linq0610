44c44c7da8c54a4b3b81fff11d60eece4f2fbc0c
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-10-06 16:09:45 +0300

    Add files via upload

---
+++ 3) Linq 33/Linq33/Linq5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 123, 45, 678, 90, 123, 45, 678, 90 };
+
+            var result = numbers
+                .Where(n => n > 0)
+                .Select(n => n % 10) 
+                .Distinct() 
+                .ToList();
+
+            foreach (var digit in result)
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
