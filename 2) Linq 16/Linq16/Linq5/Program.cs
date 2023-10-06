83b2651dcb33525b64e1bf66009b19f0e2543528
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-10-06 16:09:00 +0300

    Add files via upload

---
+++ 2) Linq 16/Linq16/Linq5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -2, 5, -10, 7, 3, -1, 0, 8 };
+
+            var positiveNumbers = numbers.Where(n => n > 0);
+
+            foreach (var number in positiveNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
